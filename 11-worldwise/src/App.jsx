440eb735455ce5bf16a5083f98654f9c75face9e
Karima-Aktari <karimaaktari911@gmail.com>
2025-08-31 12:55:30 +0600

    logo-login-sidebar

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -4,17 +4,19 @@ import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div>
-      <h1>Hi Router App!</h1>
+      {/* <h1>Hi Router App!</h1> */}
 
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
+          <Route path="login" element={<Login />} />
           <Route path="app" element={<AppLayout />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
