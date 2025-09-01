8f15149d36d81e3ede260b3a13c10fb66de81fbd
Karima-Aktari <karimaaktari911@gmail.com>
2025-09-01 11:59:28 +0600

    cityItem

--- 11-worldwise/src/components/CityItem.jsx
+++ 11-worldwise/src/components/CityItem.jsx
@@ -1,5 +1,23 @@
+import styles from "./CityItem.module.css";
+
+const formatDate = (date) =>
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    // weekday: "long",
+  }).format(new Date(date));
+
 function CityItem({ city }) {
-  return <li>City</li>;
+  const { cityName, emoji, date } = city;
+  console.log(city);
+  return (
+    <li className={styles.cityItem}>
+      <span className={styles.emoji}>{emoji}</span>
+      <h3 className={styles.name}>{cityName}</h3>
+      <time className={styles.date}>{formatDate(date)}</time>
+    </li>
+  );
 }
 
 export default CityItem;
