36e64714b906097ec1a6e4e40d5acc832d2f928a
Karima-Aktari <karimaaktari911@gmail.com>
2025-09-02 11:30:05 +0600

    city-country

--- 11-worldwise/src/components/CountryList.jsx
+++ 11-worldwise/src/components/CountryList.jsx
@@ -1,7 +1,7 @@
 import Spinner from "./Spinner";
 import styles from "./CountryList.module.css";
-import CityItem from "./CityItem";
 import Message from "./Message";
+import CountryItem from "./CountryItem";
 
 function CountryList({ cities, isLoading }) {
   if (isLoading) return <Spinner />;
@@ -11,10 +11,16 @@ function CountryList({ cities, isLoading }) {
       <Message message="Add your first city by clicking on a city on the map" />
     );
 
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country))
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    else return arr;
+  }, []);
+
   return (
-    <ul className={styles.countrieslist}>
-      {cities.map((city) => (
-        <CityItem city={city} key={city.id} />
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} key={country.id} />
       ))}
     </ul>
   );
