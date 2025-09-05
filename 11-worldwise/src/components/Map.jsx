65e03531b3fd74b8378c6c27dc381697c4e2a862
Karima-Aktari <karimaaktari911@gmail.com>
2025-09-05 20:03:48 +0600

    map

--- 11-worldwise/src/components/Map.jsx
+++ 11-worldwise/src/components/Map.jsx
@@ -1,16 +1,51 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import styles from "./Map.module.css";
+import { useState } from "react";
+import { useCities } from "../contexts/CitiesContext";
 
 function Map() {
   const navigate = useNavigate();
+  const { cities } = useCities();
+
+  const [mapPosition, setMapPosition] = useState([40, 0]);
 
   const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  // const mapLat = searchParams.get("lat");
+  // const mapLng = searchParams.get("lng");
 
   return (
-    <div className={styles.mapContainer} onClick={() => navigate("Form")}>
-      <h1> Map</h1>
+    <div
+      className={styles.mapContainer}
+      // onClick={() => navigate("Form")}
+    >
+      <MapContainer
+        center={mapPosition}
+        // center={[mapLat, mapLng]}
+        zoom={7}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span>
+              <span> {city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+
+        {/* <ChangeCenter position={[mapLat, mapLng]} /> */}
+      </MapContainer>
+
+      {/* <h1> Map</h1>
       <h1>
         Position:{lat},{lng}
       </h1>
@@ -20,9 +55,15 @@ function Map() {
         }}
       >
         Change pos
-      </button>
+      </button> */}
     </div>
   );
 }
+
+// function ChangeCenter({ position }) {
+//   const map = useMap();
+//   map.setView(position);
+//   return null;
+// }
 
 export default Map;
